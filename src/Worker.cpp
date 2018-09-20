3ef6ccb7a1f8a848f07b688a8838692b3280fd16
Hugo <hugo_jeller@hotmail.com>
2018-09-20 13:02:35 -0400

    Added Content-Type header, 404 response, and code cleanup

--- src/Worker.cpp
+++ src/Worker.cpp
@@ -23,11 +23,10 @@ Worker::Worker(Listener *listener, HTTPRequest *httpRequest) {
 
 void Worker::work() {
     char *uri = httpRequest->getRequestLine(REQUEST_URI);
-    auto *rs = new RequestSolver(uri);
-    char *response;
+    auto *request = new RequestSolver(uri);
 
     // Split request URI
-    rs->process();
+    request->process();
 
     // URI with /index.html appended to the end (default behavior)
     char *fixedUri;
@@ -35,18 +34,18 @@ void Worker::work() {
     // If request was internally redirected to CGI-BIN
     bool forceCgiBin = false;
 
-    if (rs->isDirectory()) {
-        // TODO: check for trailing slash to avoid // or no slash
-        // append index.html to request if file exists
+    if (request->isDirectory()) {
+        // Append index.html to request URI
         fixedUri = new char[strlen(uri) + strlen(INDEX_APPEND) + 1];
         strcpy(fixedUri, uri);
         strncat(fixedUri, INDEX_APPEND, sizeof(INDEX_APPEND));
 
-        // check for index.html
+        // Check if index.html exists and can be opened
         auto *indexRequest = new FileRequest(fixedUri);
         int result = indexRequest->solve();
         delete indexRequest;
 
+        // If index.html cannot be accessed, redirect to cgi-bin directory listing script
         if (result != 0) {
             delete fixedUri;
             fixedUri = new char[strlen("/cgi-bin/dir.py") + 1];
@@ -61,49 +60,55 @@ void Worker::work() {
         fixedUri = uri;
     }
 
-    if (rs->isCgiBin() || forceCgiBin) {
+    if (request->isCgiBin() || forceCgiBin) {
         response = serveCgiBin(fixedUri);
     } else {
         response = serveStaticFile(fixedUri);
     }
 
-    if (response == nullptr) {
-        printf("Could not send response to client: NULL response\n");
-    } else {
-        printf("Returning response contains %d bytes.\n", (int) strlen(response));
-        listener->sendResponse(response);
+
+    if (response == nullptr || strlen(response) == 0) {
+        printf("Could not send correct response to client: empty response. Serving 404 error\n");
+        response = serve404(uri);
     }
 
-    delete response;
-    delete rs;
+    printf("Returning response contains %d bytes.\n", responseSize);
+    listener->sendResponse(response, responseSize);
 }
 
-char *Worker::serveStaticFile(char *fixedUri) const {
-    char *response;
+char *Worker::serveStaticFile(char *fixedUri) {
     printf("Request is for static file.\n");
     auto *fr = new FileRequest(fixedUri);
     auto *rb = new HeaderBuilder();
     string *res = new string();
 
-    char *fileContent = fr->getResponse();
-
     fr->solve();
-    rb->addHeader("Content-Type", "text/html");
-    rb->addHeader("Content-Length", (char*) std::to_string(strlen(fileContent)).c_str());
 
-    char *headers = rb->render();
+    if(strstr(fixedUri, "jpeg") != nullptr || strstr(fixedUri, "jpg") != nullptr) {
+        rb->addHeader("Content-Type", "image/jpeg");
+    } else if (strstr(fixedUri, "png")) {
+        rb->addHeader("Content-Type", "image/png");
+    } else if (strstr(fixedUri, "html") || strstr(fixedUri, "css")) {
+        rb->addHeader("Content-Type", "text/html");
+    } else if (strstr(fixedUri, "svg")) {
+        rb->addHeader("Content-Type", "image/svg+xml");
+    } else {
+        rb->addHeader("Content-Type", "image/plain");
+    }
+    rb->addHeader("Content-Length", (char*) std::to_string(fr->getFileContentSize()).c_str());
 
-    res->append("HTTP/1.1 200 OK");
+    res->append("HTTP/1.1 200 OK\r\n");
     res->append(rb->render());
     res->append("\r\n");
-    res->append(fr->getResponse());
+    responseSize = (int) res->size();
+    res->append(fr->getResponse(), (unsigned long) fr->getFileContentSize());
 
-    response = (char *) res->c_str();
-    return response;
+    responseSize += fr->getFileContentSize();
+
+    return (char *) res->c_str();
 }
 
-char *Worker::serveCgiBin(char *fixedUri) const {
-    char *response;
+char *Worker::serveCgiBin(char *fixedUri) {
     printf("Request is inside CGI-BIN directory.\n");
     auto *cgi = new CgiBinRequest(fixedUri, httpRequest);
 
@@ -111,6 +116,20 @@ char *Worker::serveCgiBin(char *fixedUri) const {
     cgi->addEnvironmentVariables("QUERY_STRING", httpRequest->getQueryString());
     cgi->solve();
 
-    response = cgi->getResponse();
-    return response;
+    responseSize = (int) strlen(cgi->getResponse());
+
+    return cgi->getResponse();
+}
+
+char *Worker::serve404(char *fixedUri) {
+    string *res = new string();
+
+    res->append("HTTP/1.1 404 Not Found\r\n\r\n");
+    res->append("The requested URL ");
+    res->append(fixedUri);
+    res->append(" could not be found on this server.");
+
+    responseSize = res->size();
+
+    return (char *) res->c_str();
 }
--- src/inc/Worker.h
+++ src/inc/Worker.h
@@ -16,6 +16,12 @@ public:
     /// Connection handling class
     Listener *listener;
 
+    /// The response string
+    char *response;
+
+    /// How many bytes the response is
+    int responseSize;
+
     /// \param listener - listener and connection handling class reference
     /// \param httpRequest - request object reference
     Worker(Listener *listener, HTTPRequest *httpRequest);
@@ -23,9 +29,11 @@ public:
     /// Solves given HTTP request
     void work();
 
-    char *serveCgiBin(char *fixedUri) const;
+    char *serveCgiBin(char *fixedUri);
 
-    char *serveStaticFile(char *fixedUri) const;
+    char *serveStaticFile(char *fixedUri);
+
+    char *serve404(char *fixedUri);
 };
 
 #endif //SIMPLE_HTTP_SERVER_WORKER_H
